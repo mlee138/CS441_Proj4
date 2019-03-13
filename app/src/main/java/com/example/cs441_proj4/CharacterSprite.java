0d21893dda4c46b06dad03c0ba4c072637501bed
mlee138 <mlee138@binghamton.edu>
2019-03-12 23:58:07 -0400

    Added Sprite movement.

--- app/src/main/java/com/example/cs441_proj4/CharacterSprite.java
+++ app/src/main/java/com/example/cs441_proj4/CharacterSprite.java
@@ -1,21 +1,30 @@
 package com.example.cs441_proj4;
 
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 
 public class CharacterSprite {
 
     private Bitmap image;
+    private int x,y;
+    private int xVelocity = 10;
+    private int yVelocity = 5;
+    private int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
+    private int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
 
     public CharacterSprite(Bitmap bmp){
         image = bmp;
+        x = 100;
+        y = 100;
     }
 
     public void draw(Canvas canvas){
+
         canvas.drawBitmap(image, 100, 100, null);
     }
 
     public void update(){
-
+        y++;
     }
 }
